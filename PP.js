45521c8113c8db39f1a158f240ee8b65d4327b01
ssungs <asdf4651@naver.com>
2021-03-22 18:28:37 +0900

    feat: PP

--- PP.js
+++ PP.js
@@ -148,4 +148,28 @@ console.log(todos);
 
 // 6. 특정 요소의 프로퍼티 값 반전
 
+let todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+function toggleCompletedById(id) {
+  todos.find(idNum => idNum.id === id);
+  if (todos.find(idNum => idNum.id === id).completed === true) {
+    todos.find(idNum => idNum.id === id).completed = false;
+  } else if (todos.find(idNum => idNum.id === id).completed === false) {
+    todos.find(idNum => idNum.id === id).completed = true;
+  }
+}
+toggleCompletedById(1);
+console.log(todos);
+/*
+[
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: false },
+  { id: 1, content: 'Javascript', completed: false }
+]
+*/
+
+// 7. 모든 요소의 completed 프로퍼티 값을 true로 설정
 
