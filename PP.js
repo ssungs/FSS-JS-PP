96460259d089db85d1480ac31845cfbbc4a4b7e6
ssungs <asdf4651@naver.com>
2021-03-22 18:07:54 +0900

    feat: PP

--- PP.js
+++ PP.js
@@ -29,6 +29,18 @@ function render() {
 
 console.log(render());
 
+/*
+<li id="3">
+  <label><input type="checkbox">HTML</label>
+</li>
+<li id="2">
+  <label><input type="checkbox" checked>CSS</label>
+</li>
+<li id="1">
+  <label><input type="checkbox">Javascript</label>
+</li>
+*/
+
 // 2. 특정 프로퍼티 값 추출
 
 const todos = [
@@ -42,4 +54,97 @@ function getValues(key) {
 console.log(getValues('id')); // [3, 2, 1]
 console.log(getValues('content')); // ['HTML', 'CSS', 'Javascript']
 console.log(getValues('completed')); // [false, true, false]
+
+// 3. 프로퍼티 정렬
+
+const todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+function sortBy(key) {
+  let arr = todos.slice();
+  arr.sort((a, b) => a - b);
+  return arr;
+}
+
+console.log(sortBy('id'));
+
+/*
+[
+  { id: 1, content: 'Javascript', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 3, content: 'HTML', completed: false }
+]
+*/
+
+console.log(sortBy('content'));
+
+/*
+[
+  { id: 2, content: 'CSS', completed: true },
+  { id: 3, content: 'HTML', completed: false },
+  { id: 1, content: 'Javascript', completed: false }
+]
+*/
+
+console.log(sortBy('completed'));
+
+/*
+[
+  { id: 3, content: 'HTML', completed: false },
+  { id: 1, content: 'Javascript', completed: false },
+  { id: 2, content: 'CSS', completed: true }
+]
+*/
+
+// 4. 새로운 요소 추가
+
+let todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+function addTodo(newTodo) {
+  todos.splice(0, 0, newTodo);
+}
+
+addTodo({ id: 4, content: 'Test', completed: false });
+
+console.log(todos);
+/*
+[
+  { id: 4, content: 'Test', completed: false },
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+]
+*/
+
+// 5. 특정 요소 삭제
+
+let todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+function removeTodo(id) {
+  todos.splice(todos.findIndex(idNum => idNum.id === id), 1);
+}
+
+removeTodo(2);
+
+console.log(todos);
+
+/*
+[
+  { id: 3, content: 'HTML', completed: false },
+  { id: 1, content: 'Javascript', completed: false }
+]
+*/
+
+// 6. 특정 요소의 프로퍼티 값 반전
 
