601c5f326e0bd706bc9eb30878ea4eaedbbac1b6
ssungs <asdf4651@naver.com>
2021-03-22 15:32:26 +0900

    feat: PP

---
+++ PP.js
@@ -0,0 +1,32 @@
+// 1. html 생성
+
+const todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+function render() {
+  let html = '';
+
+  todos.forEach(todo => {
+    if (todo.id !== 2) {  
+      html += 
+      `<li id="${todo.id}">
+        <label><input type="checkbox">${todo.content}</label>
+      </li>`;
+    }
+    else if (todo.id === 2) {  
+      html +=
+      `<li id="${todo.id}">
+        <label><input type="checkbox" checked>${todo.content}</label>
+      </li>`;
+    }
+  });
+
+  return html;
+}
+
+console.log(render());
+
+// 2. 특정 프로퍼티 값 추출
